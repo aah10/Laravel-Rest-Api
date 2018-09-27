b2d7d2a7c50f2b47a9f91e14b49110efe825a34c
aah10 <misbahudinaah10>
2018-09-27 13:56:22 +0700

    upload di git-hub

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+
+        $faker = Faker\Factory::create();
+
+        $limit = 5 ;
+
+        for ($i=0; $i < $limit ; $i++) { 
+        	DB::table('kontak')->insert([
+        		'nama' => $faker->name,
+        		'email' => $faker->unique()->email,
+        		'nohp' => $faker->phoneNumber,
+        		'alamat' => $faker->address,
+        	]);
+        }
+    }
+}
